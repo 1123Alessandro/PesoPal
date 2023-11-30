b6d1110d0b43f7df38d114197f7a2333302f8105
1123Alessandro <alessandroandyrei@gmail.com>
2023-12-01 05:47:01 +0800

    Created dart files for transaction models

---
+++ lib/model/Earn.dart
@@ -0,0 +1,20 @@
+import 'Transaction.dart';
+import 'TxnType.dart';
+import 'dart:core';
+
+class Earn extends Transaction {
+
+  TxnType type = TxnType.Earn;
+
+  Earn({required name, required price, required date}) : super(name: name, price: price, date: date);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': this.getID(),
+      'name': this.getName(),
+      'price': this.getPrice(),
+      'date': this.getDate(),
+      'type': 'Earn',
+    };
+  }
+}
